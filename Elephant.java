0fad2543e5715b6c6171221484f8231aa97b89d7
JoshPham <JTPham911@gmail.com>
2024-02-13 06:56:43 +0000

    finished Elephant subclass

--- Elephant.java
+++ Elephant.java
@@ -4,4 +4,18 @@ public class Elephant extends Animal {
     public Elephant() {
         super("plants", false, 50);
     }
+
+    @Override
+    public void eat() {
+        System.out.println("While normally being too tall for normal animals, the elephant\nuses its trunk to grab the " + super.getFood() + " and eats it.");
+    }
+
+    public void trumpet() {
+        System.out.println("The elephant uses its trunks to trumpet, and it can be heard from miles away.");
+    }
+
+    public void forage() {
+        System.out.println("In the grass fields, the elephant finds a " + super.getFood() + " and gathers it along with the nearby vegetation.");
+        eat();
+    }
 }
